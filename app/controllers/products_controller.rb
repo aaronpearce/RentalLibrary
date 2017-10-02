a17f6a06193f72001445728eb0e78d69e6acdbeb
Jono <jono.newton@gmail.com>
2017-10-02 17:22:11 +1300

    Products Page update/delete

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,56 @@
+class ProductsController < ApplicationController
+
+	before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  	def index
+    	@products = Product.all
+  	end
+
+  	def show
+  	end
+
+  	def new
+    	@product = Product.new
+  	end
+
+  	def create
+    	@product = Product.new(product_params)
+    	if @product.save
+      		redirect_to products_path, notice: "Successfully Created"
+    	else
+      		flash[:error] =  @product.errors.full_messages.to_sentence
+      		render action: 'new'
+    	end
+  	end
+  
+ 	 def edit
+    	@page_title = t('product.edit', product_name: @product.name)
+  	end
+
+  	def update
+    	if @product.update(product_params)
+      		respond_to do |format|
+        		format.html { redirect_to product_path(@product), notice: "Successfully Updated" }
+        		format.json { render json: { success: true } }
+      		end
+    	else
+      		flash[:error] = @product.errors.full_messages.to_sentence
+      		render action: 'edit'
+		end
+  	end 
+
+  	def destroy
+    	@product.destroy
+   	 	redirect_to products_path()
+  	end
+
+  	private
+
+  	def set_product
+   	 	@product = Product.find(params[:id])
+  	end
+
+  	def product_params
+   		params.require(:product).permit(:name, :description, :rented, :hirefee)
+  	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001212534) do
+ActiveRecord::Schema.define(version: 20171001224124) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20171001212534) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_models_on_email", unique: true
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.boolean "rented"
+    t.integer "hirefee"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

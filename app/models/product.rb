dfe90bed3219bc13bf5d8b3cc443c155982a82bb
Aaron Pearce <me@aaronpearce.com>
2017-10-03 10:02:09 +1300

    Add hirefee setter

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,8 @@
 class Product < ActiveRecord::Base
-	before_validation :dollar_to_cent
-
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "50x50>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
-	def dollar_to_cent
-		Rails.logger.info self.hirefee
-      self.hirefee = self.hirefee * 100.0 
-		Rails.logger.info self.hirefee
-    end
+		
+	def hirefee=(value)
+			write_attribute :hirefee, value.to_d * 100.0 
+  end
 end
